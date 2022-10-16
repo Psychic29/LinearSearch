7d6987598b33d1efe76cd7f19fd8b7edf7f6d273
Vedasto <prajesh.eleven118@gmail.com>
2022-10-16 15:29:43 +0530

    - added c file - added .gitignore file

--- linear.java
+++ linear.java
@@ -12,14 +12,19 @@ class linear {
     public static void main(String args[])
     {
         int arr[] = { 2, 3, 4, 10, 40 };
-        int x = 50;
-
+        int x = 3;
+        System.out.print("[*] The elements of the array are: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        System.out.println("[*] The element to be searched is: " + x);
         int result = search(arr, x);
         if (result == -1)
             System.out.print(
-                "Element is not present in array");
+                "[!] Element is not present in array\n");
         else
-            System.out.print("Element is present at index "
-                             + result);
+            System.out.print("[+] Element is present at index "
+                             + result + ".\n");
     }
 }
