0ac8798ba46fa6b7fe8c0a3a3e36edb5d8fade80
Psychic29 <115920922+Psychic29@users.noreply.github.com>
2022-10-16 16:17:38 +0530

    Revert "Modified existing and added C language code"

--- linear.java
+++ linear.java
@@ -8,23 +8,18 @@ class linear {
         }
         return -1;
     }
-
+ 
     public static void main(String args[])
     {
         int arr[] = { 2, 3, 4, 10, 40 };
-        int x = 3;
-        System.out.print("[*] The elements of the array are: ");
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
-        System.out.println("[*] The element to be searched is: " + x);
+        int x = 50;
+ 
         int result = search(arr, x);
         if (result == -1)
             System.out.print(
-                "[!] Element is not present in array\n");
+                "Element is not present in array");
         else
-            System.out.print("[+] Element is present at index "
-                             + result + ".\n");
+            System.out.print("Element is present at index "
+                             + result);
     }
 }
